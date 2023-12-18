b624f738498ae74df441ee7cd6ae11da4a2f8a98
zac_2203 <zachew2203@gmail.com>
2023-12-18 09:12:30 -0500

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,9 +9,8 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
-    # path for about view
-
-    # path for contact us view
+    path(route='about', view=views.about, name='about'),
+    path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
 
